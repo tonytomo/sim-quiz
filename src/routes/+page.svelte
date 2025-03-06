5f9fa6dd96c42ede44adc69b435205dad0b861d9
tonytomo <fathonisatrioutomo@gmail.com>
2025-03-06 11:08:07 +0700

    initialize SvelteKit Quiz project with Tailwind CSS and ESLint configuration

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<h1 class="font-thin">Welcome to the Quiz App!</h1>
+<p class="mt-4">
+	This is a simple quiz app that I built to learn Svelte. It's a work in progress, so please be
+	patient with me.
+</p>
