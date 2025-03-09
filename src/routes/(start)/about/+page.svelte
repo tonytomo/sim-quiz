e96c2f644baf1a7f63fcb86a171da7fefc94efcd
tonytomo <fathonisatrioutomo@gmail.com>
2025-03-09 18:32:20 +0700

    feat: update About page with detailed description and table of contents for SimQuiz

--- src/routes/(start)/about/+page.svelte
+++ src/routes/(start)/about/+page.svelte
@@ -4,7 +4,47 @@
 
 <Header isBack />
 
-<main class="container-stack gap-2">
-	<h1 class="big-title">About</h1>
-	<h2 class="subtitle">Upload a quiz file with questions and answers to get started.</h2>
+<main class="container-blog my-20 gap-4 scroll-smooth">
+	<section class="container-blog gap-10 p-4">
+		<h1 class="big-title">What is SimQuiz?</h1>
+		<h2 class="subtitle">
+			SimQuiz is a simple quiz app that allows you to create and share quizzes with your friends.
+			You can also use it to study for your exams or to test your knowledge on a specific topic.<br
+			/>
+			<br />
+			In simple terms, SimQuiz is a quiz simulator.
+		</h2>
+	</section>
+
+	<hr />
+
+	<section class="container-blog gap-2 p-4">
+		<div class="container-color">
+			<h2 class="title">Table of contents</h2>
+			<ul class="list-inside list-decimal text-sm">
+				<li><a href="#welcomer">Welcomer</a></li>
+				<li><a href="#how-to-use-simquiz">How to use SimQuiz?</a></li>
+				<li><a href="#how-to-create-a-quiz">How to create a quiz?</a></li>
+				<li><a href="#how-to-share-a-quiz">How to share a quiz?</a></li>
+				<li><a href="#how-to-study-with-simquiz">How to study with SimQuiz?</a></li>
+				<li><a href="#how-to-use-simquiz-for-fun">How to use SimQuiz for fun?</a></li>
+				<li><a href="#how-to-use-simquiz-for-learning">How to use SimQuiz for learning?</a></li>
+				<li><a href="#how-to-use-simquiz-for-education">How to use SimQuiz for education?</a></li>
+				<li><a href="#how-to-use-simquiz-for-business">How to use SimQuiz for business?</a></li>
+				<li>
+					<a href="#how-to-use-simquiz-for-competition">How to use SimQuiz for competition?</a>
+				</li>
+			</ul>
+		</div>
+	</section>
+
+	<section class="container-blog gap-2 p-4" id="welcomer">
+		<h2 class="title">Welcomer</h2>
+		<p class="subtitle">
+			Welcome to SimQuiz! SimQuiz is a simple quiz app that allows you to create and share quizzes
+			with your friends. You can also use it to study for your exams or to test your knowledge on a
+			specific topic. It is also can be used for fun, learning, education, business, and simple
+			competition.
+		</p>
+	</section>
 </main>
