afedc3e757015ab840b56aaa7dd8857b943e97d5
tonytomo <fathonisatrioutomo@gmail.com>
2025-07-09 11:12:17 +0700

    refactor: clean up deploy.yml and svelte.config.js for improved readability

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const dev = process.argv.includes('dev');
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
@@ -12,12 +10,8 @@ const config = {
 	kit: {
 		adapter: adapter({
 			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}),
-		paths: {
-			base: dev ? '' : process.env.BASE_PATH
-		}
+			assets: 'build'
+		})
 	}
 };
 
